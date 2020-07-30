9300db391b7bcb9115fb7447dc6e2273c6c6b034
owaish <owaissheikh76@gmail.com>
2020-07-30 17:31:01 +0530

    second commit

---
+++ PatternReverse2/src/Reserve.java
@@ -0,0 +1,15 @@
+
+public class Reserve {
+	public static void main(String args[]) {
+		int i,j;
+		for(i=5;i>=1;i--) {
+			for(j=5; j>=1;j--)
+			{
+			System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	
+	}
+
+}
